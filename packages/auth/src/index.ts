b23e7410b52c0d22fe530b8f5a319db79cf6371a
Neamat Khan Mim <neamatmim@gmail.com>
2026-09-10 11:00:29 +0600

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { createDb } from "@OpenFarm/db";
+import * as schema from "@OpenFarm/db/schema/auth";
+import { env } from "@OpenFarm/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.BETTER_AUTH_URL],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [tanstackStartCookies()],
+  });
+}
+
+export const auth = createAuth();
